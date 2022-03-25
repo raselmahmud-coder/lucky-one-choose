1c6a05e0a5e65e23dd7458851a64eaec872492fc
RASEL MAHMUD <raselmahmud98262@gmail.com>
2022-03-26 03:15:59 +0800

    Header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            <h1>Hello jsx</h1>
+        <Navbar bg="light" expand="lg">
+       <Container>
+        <Navbar.Brand href="#home">Burger Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+       </Container>
+            </Navbar>
+            <h1 className='text-center'>Burger Shop</h1>
         </div>
     );
 };
